36450886b4ff59a84a08664364511b893553631f
Ricardo Zapata <ricardozap98@hotmail.com>
2024-11-07 17:16:13 -0600

    Finalizando parcialmente proyecto calcudadora de propinas

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -4,22 +4,37 @@ import type { MenuItem, OrderItem } from "../types"
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
+    const [tip, setTip] = useState(0)
     
     const addItem = (item : MenuItem) => {
         const itemExist = order.find(orderItem => orderItem.id === item.id)
         if(itemExist){
-            console.log("ya existe")
+            const updatedOrder = order.map( orderItem => orderItem.id === item.id ? 
+                {...orderItem, quantity: orderItem.quantity + 1}  : 
+                orderItem
+            )
+            setOrder(updatedOrder)
         } else {
             const newItem = {...item, quantity: 1}
             setOrder([...order, newItem])
         }
-
-
     }
 
-    console.log(order)
+    const removeItem = (id: MenuItem['id']) => {
+        setOrder(order.filter( item => item.id !== id))
+    }
+
+    const placeOrder = () => {
+        setOrder([])
+        setTip(0)
+    }
 
     return {
-        addItem
+        order,
+        tip,
+        setTip,
+        addItem,
+        removeItem,
+        placeOrder
     }
 }
